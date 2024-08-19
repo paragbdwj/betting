94a4906d87e14ff83c08481a5c6c7325a6768f34
paragbdwj <paragbhardwajcs@gmail.com>
2024-08-19 19:53:23 +0530

    changes-in-master

--- src/main/java/com/elephants/betting/common/filter/WebConfig.java
+++ src/main/java/com/elephants/betting/common/filter/WebConfig.java
@@ -1,5 +1,6 @@
 package com.elephants.betting.common.filter;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,10 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${allowed.origins}")
+    private String allowedOrigins;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // Change this to your frontend URL
+                .allowedOrigins(allowedOrigins) // Change this to your frontend URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Content-Type", "Authorization")
                 .allowCredentials(true)
