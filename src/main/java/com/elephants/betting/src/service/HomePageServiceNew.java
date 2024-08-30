d1cc51fa45d751067221032958cbc7e0205ca675
paragbdwj <paragbhardwajcs@gmail.com>
2024-08-30 12:20:21 +0530

    added-filter-query

--- src/main/java/com/elephants/betting/src/service/HomePageServiceNew.java
+++ src/main/java/com/elephants/betting/src/service/HomePageServiceNew.java
@@ -14,7 +14,7 @@ public class HomePageServiceNew {
         public HomePageResponse getHomePageAPI(HomePageRequest request) {
             return HomePageResponse.builder()
 //                    .matches(scheduler.getMatchesToShowOnHomePage().stream().sorted((a,b)-> Boolean.compare(b.getIsLiveMatch(), a.getIsLiveMatch())).toList())
-                    .matches(scheduler.getMatchesToShowOnHomePage().stream().filter(cricketMatches -> Boolean.TRUE.equals(cricketMatches.getIsLiveMatch())).toList())
+                    .matches(scheduler.getMatchesToShowOnHomePage().stream().filter(cricketMatches -> Boolean.TRUE.equals(cricketMatches.getIsLiveMatch()) && Boolean.FALSE.equals(cricketMatches.isTestMatch())).toList())
                     .build();
 
         }
